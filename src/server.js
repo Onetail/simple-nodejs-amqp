a082ba9f2bdfbc61c44e2066145b6df2fef18074
onetail <paosong91@gmail.com>
2019-08-11 16:27:56 +0800

    initilize server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,22 @@
 const amqp = require('amqplib/callback_api');
+const { envs } = require(`./config/${process.env.NODE_ENV || 'default'}.js`);
+
+amqp.connect(`amqp://${envs.SERVER_IP}`, function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var queue = 'hello';
+        var msg = 'Hello world';
+
+        channel.assertQueue(queue, {
+            durable: false,
+        });
+
+        channel.sendToQueue(queue, Buffer.from(msg));
+        console.log(' [x] Sent %s', msg);
+    });
+});
