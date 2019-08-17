0b4b2cfd776d968ff18a172fb90469e24e7c31d2
onetail <paosong91@gmail.com>
2019-08-17 20:40:57 +0800

    add client.js to receive message

---
+++ src/client.js
@@ -0,0 +1,31 @@
+var amqp = require('amqplib/callback_api');
+const { envs } = require(`./config/${process.env.NODE_ENV || 'default'}.js`);
+
+amqp.connect(`amqp://${envs.SERVER_USERNAME}:${envs.SERVER_PASSWORD}@${envs.SERVER_IP}`, function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        var queue = 'hello';
+
+        channel.assertQueue(queue, {
+            durable: false,
+        });
+
+        console.log(' [*] Waiting for messages in %s. To exit press CTRL+C', queue);
+
+        channel.consume(
+            queue,
+            function(msg) {
+                console.log(' [x] Received %s', msg.content.toString());
+            },
+            {
+                noAck: true,
+            }
+        );
+    });
+});
