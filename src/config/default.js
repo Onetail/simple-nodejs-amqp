469fcf1d5fe41e6c77df0b701ade7eb21a0e9397
onetail <paosong91@gmail.com>
2020-03-22 11:28:36 +0800

    adjust docker-compose and config

--- src/config/default.js
+++ src/config/default.js
@@ -1,6 +1,6 @@
 module.exports = {
     envs: {
-        SERVER_IP: 'localhost',
+        SERVER_IP: process.env.RABBITMQ_HOST || '127.0.0.1',
         SERVER_USERNAME: 'root',
         SERVER_PASSWORD: 'root',
         CONNECT_TIMEOUT: 1500,
